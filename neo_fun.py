28f136ce57ad839a81a5ea8ba5ebb25186599c33
Shaun Horine <shorine@squarespace.com>
2019-10-23 15:03:12 -0700

    Stubbed out some notes and a plan?

---
+++ neo_fun.py
@@ -0,0 +1,22 @@
+# Let's build a python application to pull some info on Near Earth Objects and give it back to people in interesting ways
+
+import request
+import json
+import pprint
+
+# TODO: Build a method to pull the feed data based on user input start/end dates
+
+# TODO: Parse the data to return data for...
+
+# TODO: ...Furthest NEO
+
+# TODO: ...Nearest NEO
+
+# TODO: ...Largest (diameter) NEO
+
+# TODO: ...Fastest NEO
+
+# TODO: Any NEO classified as PHA (potentially hazardous)
+
+
+
