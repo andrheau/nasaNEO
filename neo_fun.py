15e497c783fd0f15b5279777cf143c26c91df686
Shaun Horine <shorine@squarespace.com>
2019-10-23 16:28:24 -0700

    Started collecting dates and trying to prevent letters. No good

--- neo_fun.py
+++ neo_fun.py
@@ -9,9 +9,15 @@ import json
 Defined variables for start/end date
 """
 start_date = input("Please enter a starting date (Format: YYYY-MM-DD): ")
+while start_date is not int:
+    start_date = input("Ouch, reading instructions isn't your favorite eh? Let's make sure that date is in this format \n (YYYY-MM-DD): ")
+
 end_date = input("Now enter an end date (Format: YYYY-MM-DD): ")
-    
-print(start_date, end_date)
+
+while end_date is not int:
+    end_date = input("Ouch, reading instructions isn't your favorite eh? Let's make sure that date is in this format \n (YYYY-MM-DD): ")
+
+print("Start Date: {} | End Date: {}").format(start_date, end_date)
 # TODO: Parse the data to return data for...
 
 # TODO: ...Furthest NEO
