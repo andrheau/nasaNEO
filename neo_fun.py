6f55e25eba93fb119f41bd535b933b5493a6a166
Shaun Horine <shorine@squarespace.com>
2019-10-25 13:43:41 -0700

    Now with declared variables for lentgh of armadillos and deloreans in meters

--- neo_fun.py
+++ neo_fun.py
@@ -13,9 +13,16 @@ apiKey = "13L6o25sYaUm7Ac1Op2e09FrWudmH6N0rRhVBoCx"
 # Get and return data
 response = requests.get(api_url + apiKey)
 
+# Set variables based on data
 data = response.text
 parsed = json.loads(data)
 neos = parsed["near_earth_objects"]["2019-10-25"]
+
+# Set global variables
+
+length_of_armadillo = 1.5
+length_of_delorean = 4.2
+
 # max_diameter = neos["estimated_diameter"]["meters"]["estimated_diameter_max"]
 
 # TODO: Parse the data to return data for...
@@ -41,13 +48,15 @@ largest = None
 for diameter in neos:
     max_diameter = round(diameter["estimated_diameter"]["meters"]["estimated_diameter_max"])
     name = diameter["name"]
+    diameter_in_armadillos = round(max_diameter / length_of_armadillo)
+    diameter_in_deloreans = round(max_diameter / length_of_delorean)
     if largest is None:
         largest = max_diameter
     if largest > max_diameter:
         pass
     else:
         largest = max_diameter
-print("{} is the largest NEO today at a whopping {} meters in diameter!".format(name, largest))
+print("{} is the largest NEO today at a whopping {} meters in diameter! \nThat means it's {} armadillos OR {} Deloreans in diameter!".format(name, largest, diameter_in_armadillos, diameter_in_deloreans))
 
 
 #    print (max_diameter)
