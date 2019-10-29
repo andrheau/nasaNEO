37aee825f1c0a1e7757a1bf556ee564570367cdd
Andrhea Unger <andrheau@gmail.com>
2019-10-29 14:14:00 -0700

    Now with more Neo in our NEO and an unhelpful help prompt

--- nasatest.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import json
-import pprint
-contents = urllib.request.urlopen("https://api.nasa.gov/neo/rest/v1/neo/3879283?api_key=13L6o25sYaUm7Ac1Op2e09FrWudmH6N0rRhVBoCx").read()
-varab = json.loads(contents)
-
-pprint.pprint(('object 3802394 is: ')+varab['close_approach_data'][0]['miss_distance']['miles']+(" miles away!"))
